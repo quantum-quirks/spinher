5ce19e7cd6c4cf6918558c39659f4a7dc527dc6c
Gerrishon Sirere <34297473+viewerdiscretion@users.noreply.github.com>
2021-02-15 00:27:05 +0300

    Update __init__.py

--- src/spinher/__init__.py
+++ src/spinher/__init__.py
@@ -12,18 +12,18 @@ import quo
 class Whirl(object):
     sequence = itertools.cycle(['-', '/', '_', '|', '\\'])
 
-    def initialize(solitary, sound=False, disable=False, force=False, stream=sys.stdout):
+    def initialize(solitary, sound=False, disable=False, force=False, flow=sys.stdout):
         solitary.sound = sound
         solitary.disable = disable
         solitary.force = force
-        solitary.stream = stream
+        solitary.flow = flow
         solitary.stop_running = None
         solitary.spin_thread = None
 
     def start(solitary):
         if solitary.disable:
             return
-        if solitary.stream.isatty() or solitary.force:
+        if solitary.flow.isatty() or solitary.force:
             solitary.stop_running = threading.Event()
             solitary.spin_thread = threading.Thread(target=solitary.initialize_spinner)
             solitary.spin_thread.start()
@@ -35,11 +35,11 @@ class Whirl(object):
 
     def initialize_spinner(solitary):
         while not solitary.stop_running.is_set():
-            solitary.stream.write(next(solitary.sequence))
-            solitary.stream.flush()
+            solitary.flow.write(next(solitary.sequence))
+            solitary.flow.flush()
             solitary.stop_running.wait(0.2)
-            solitary.stream.write('\b')
-            solitary.stream.flush()
+            solitary.flow.write('\b')
+            solitary.flow.flush()
 
     def enter(solitary):
         solitary.start()
@@ -50,12 +50,12 @@ class Whirl(object):
             return False
         solitary.stop()
         if solitary.sound:
-            solitary.stream.write('\7')
-            solitary.stream.flush()
+            solitary.flow.write('\7')
+            solitary.flow.flush()
         return False
 
-def whirl(sound=False, disable=False, force=False, stream=sys.stdout):
-    return Whirl(sound, disable, force, stream)
+def whirl(sound=False, disable=False, force=False, flow=sys.stdout):
+    return Whirl(sound, disable, force, flow)
 
 
 __version__ = "2021.2.dev3"
