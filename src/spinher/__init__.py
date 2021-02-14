fc42c252f8ce24de71d192fe18c89a85eae43a4f
Gerrishon Sirere <34297473+viewerdiscretion@users.noreply.github.com>
2021-02-14 23:10:08 +0300

    Update __init__.py

--- src/spinher/__init__.py
+++ src/spinher/__init__.py
@@ -55,29 +55,6 @@ class Whirl(object):
         return False
 
 def whirl(beep=False, disable=False, force=False, stream=sys.stdout):
-    """This function creates a context manager that is used to display a
-    spinner on stdout as long as the context has not exited.
-
-    The spinner is created only if stdout is not redirected, or if the spinner
-    is forced using the `force` parameter.
-
-    Parameters
-    ----------
-    beep : bool
-        Beep when spinner finishes.
-    disable : bool
-        Hide spinner.
-    force : bool
-        Force creation of spinner even when stdout is redirected.
-
-    Example
-    -------
-
-        with spinher():
-            open_file()
-            check_wordcount()
-
-    """
     return Whirl(beep, disable, force, stream)
 
 
